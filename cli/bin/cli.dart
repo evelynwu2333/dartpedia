2ce275a728df0be75d2d4169a574c82fa4d7886d
Evelyn Wu <evelynwu@Mac-2.modem>
2026-03-17 09:54:26 +1100

    First commit

---
+++ cli/bin/cli.dart
@@ -0,0 +1,25 @@
+const version = '0.0.1';
+void main(List<String> arguments) {
+  if (arguments.isEmpty || arguments.first == 'help') {
+    printUsage();
+  } else if (arguments.first == 'version') {
+    print('Dartpedia CLI version $version');
+  } else if (arguments.first == 'search') {
+    print('Search command recognized!');
+    final inputArgs = arguments.length > 1? arguments.sublist(1): null;
+    searchWidipedia(inputArgs);
+  } else {
+    printUsage();
+  } 
+}
+
+
+void searchWidipedia(List<String> arguments) {
+  print('searchWikipedia received arguments: $arguments');
+}
+
+void printUsage() {
+  print(
+    "The following comands are valid: 'help', 'version', 'search <ARTICLE-TITLE>'"
+  );
+}
